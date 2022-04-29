226881593136993619db229c5b2476539557e049
Eid Liz <eeid1@woolworths.com.au>
2022-04-29 18:03:23 +1000

    num.AC.Del keys find/connectfunction/display

--- calc.js
+++ calc.js
@@ -1,93 +1,78 @@
-// Find the button
-const numbersButtonDivide = document.getElementById("numbersButtonDivide");
-{
-    numbersButtonDivide.innerText;
+//finding the key and connecting it to the buttn - but not dispalying them yet
+const numbersKeys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < numbersKeys.length; i++) {
+    const number = numbersKeys[i];
+    const button = document.getElementById("numbersButton" + number);
+    button.addEventListener("click", () => numberInput(number));
 }
-const numbersButton1 = document.getElementById("numbersButton1");
-{
-    numbersButton1.innerText;
-}
-const numbersButton2 = document.getElementById("numbersButton2");
-{
-    numbersButton2.innerText;
-}
-const numbersButton3 = document.getElementById("numbersButton3");
-{
-    numbersButton3.innerText;
-}
-const numbersButton4 = document.getElementById("numbersButton4");
-{
-    numbersButton4.innerText;
-}
-const numbersButton5 = document.getElementById("numbersButton5");
-{
-    numbersButton5.innerText;
-}
-const numbersButton6 = document.getElementById("numbersButton6");
-{
-    numbersButton6.innerText;
-}
-const numbersButton7 = document.getElementById("numbersButton7");
-{
-    numbersButton7.innerText;
-}
-const numbersButton8 = document.getElementById("numbersButton8");
-{
-    numbersButton8.innerText;
-}
-const numbersButton9 = document.getElementById("numbersButton9");
-{
-    numbersButton9.innerText;
-}
-const numbersButton0 = document.getElementById("numbersButton0");
-{
-    numbersButton0.innerText;
-}
-//button funtions
 
-const numberInputDevide = () => {
-    document.getElementById("previousValue").innerText += "÷";
-};
-const numberInput1 = () => {
-    document.getElementById("previousValue").innerText += 1;
-};
-const numberInput2 = () => {
-    document.getElementById("previousValue").innerText += 2;
-};
-const numberInput3 = () => {
-    document.getElementById("previousValue").innerText += 3;
-};
-const numberInput4 = () => {
-    document.getElementById("previousValue").innerText += 4;
-};
-const numberInput5 = () => {
-    document.getElementById("previousValue").innerText += 5;
-};
-const numberInput6 = () => {
-    document.getElementById("previousValue").innerText += 6;
-};
-const numberInput7 = () => {
-    document.getElementById("previousValue").innerText += 7;
-};
-const numberInput8 = () => {
-    document.getElementById("previousValue").innerText += 8;
-};
-const numberInput9 = () => {
-    document.getElementById("previousValue").innerText += 9;
-};
-const numberInput0 = () => {
-    document.getElementById("previousValue").innerText += 0;
+// let buttonOperator = document.querySelectorAll(".btnOperator");
+// buttonOperator.forEach((operator) => {
+//     operator.addEventListener("click", () => {
+//         t
+//     });
+// });
+
+// const operatorKeys = ["Plus", "Divide", "x", "/"];
+// for (let i = 0; i < operatorKeys.length; i++) {
+//     const operator = operatorKeys[i];
+//     const button = document.getElementById("numbersButton" + operator);
+//     button.addEventListener(
+//         "click",
+//         () => (calculateOperations = (n1, operator, n2))
+//     );
+// }
+
+// funtions
+
+//display on the dispay
+const display = (number) => {
+    document.getElementById("displayValue").innerText = number;
+    console.log(number);
 };
 
-//connecting to the buttons
-numbersButtonDivide.addEventListener("click", numberInputDevide);
-numbersButton1.addEventListener("click", numberInput1);
-numbersButton2.addEventListener("click", numberInput2);
-numbersButton3.addEventListener("click", numberInput3);
-numbersButton4.addEventListener("click", numberInput4);
-numbersButton5.addEventListener("click", numberInput5);
-numbersButton6.addEventListener("click", numberInput6);
-numbersButton7.addEventListener("click", numberInput7);
-numbersButton8.addEventListener("click", numberInput8);
-numbersButton9.addEventListener("click", numberInput9);
-numbersButton0.addEventListener("click", numberInput0);
+let n1 = ""; // will be stored after they hit an operator
+let n2 = ""; // will be stored after they click equals
+let operator = ""; // will be stored agter they hit an operator
+let result = "";
+
+// number 0-9 function
+let number = "";
+const numberInput = (n) => {
+    number += n;
+    display(number);
+};
+
+//backspace function
+const backspace = () => {
+    number = number.slice(0, number.length - 1);
+    display(number);
+};
+
+//AC function
+const clear = () => {
+    number = "";
+    display(number);
+};
+
+//Euqals Functions
+const equals = (n1, operator, n2) => {
+    let result = "";
+    if (operator === "+") {
+        result = n1 + n2;
+    } else if (operator === "-") {
+        result = n1 - n2;
+    } else if (operator === "x") {
+        result = n1 * n2;
+    } else if (operator === "/") {
+        result = n1 / n2;
+    }
+
+    return result;
+};
+
+const buttonClr = document.getElementById("buttonClr");
+buttonClr.addEventListener("click", () => clear());
+
+const buttonBkspc = document.getElementById("buttonBkspc");
+buttonBkspc.addEventListener("click", () => backspace());
---
+++ extrajs.js
@@ -0,0 +1,64 @@
+const numbersButton1 = document.getElementById("numbersButton1");
+numbersButton1.addEventListener("click", () => numberInput("1"));
+// const numbersButton2 = document.getElementById("numbersButton2");
+// const numbersButton3 = document.getElementById("numbersButton3");
+// const numbersButton4 = document.getElementById("numbersButton4");
+// const numbersButton5 = document.getElementById("numbersButton5");
+// const numbersButton6 = document.getElementById("numbersButton6");
+// const numbersButton7 = document.getElementById("numbersButton7");
+// const numbersButton8 = document.getElementById("numbersButton8");
+// const numbersButton9 = document.getElementById("numbersButton9");
+// const numbersButton0 = document.getElementById("numbersButton0");
+
+const storeNumber1 = () => {
+    let number1 = document.getElementById("previousValue").value;
+    for (i = 0, i = 0; i < calcButton.length; i++) {
+        return number1[i];
+    }
+};
+//    return(Number1)
+//
+
+// function returnText(){
+
+// }
+
+// const storeNumber1 = () => {
+//     let Number1 = document.getElementById("previousValue").value;
+//    return(Number1)
+// }
+
+// let storenumbers = []
+// class class
+// const n1 = () => {
+//     for()
+// }
+
+//button functions:
+const numberInput2 = () => {
+    document.getElementById("previousValue").innerText += 2;
+};
+const numberInput3 = () => {
+    document.getElementById("previousValue").innerText += 3;
+};
+const numberInput4 = () => {
+    document.getElementById("previousValue").innerText += 4;
+};
+const numberInput5 = () => {
+    document.getElementById("previousValue").innerText += 5;
+};
+const numberInput6 = () => {
+    document.getElementById("previousValue").innerText += 6;
+};
+const numberInput7 = () => {
+    document.getElementById("previousValue").innerText += 7;
+};
+const numberInput8 = () => {
+    document.getElementById("previousValue").innerText += 8;
+};
+const numberInput9 = () => {
+    document.getElementById("previousValue").innerText += 9;
+};
+const numberInput0 = () => {
+    document.getElementById("previousValue").innerText += 0;
+};
